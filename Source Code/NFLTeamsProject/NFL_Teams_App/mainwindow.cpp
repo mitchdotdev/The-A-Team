694b7f53330ec721825067485972e4442413bc36
Jake Sichley <39101429+JakeSichley@users.noreply.github.com>
2018-11-14 01:38:22 -0800

    Let's Get Hurt Yet Again
    Code for Story #1 & #2

--- Source Code/NFLTeamsProject/NFL_Teams_App/mainwindow.cpp
+++ Source Code/NFLTeamsProject/NFL_Teams_App/mainwindow.cpp
@@ -8,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     ui->stackedWidget->setCurrentIndex(0);
     ui->stackedWidget->currentWidget()->setWindowOpacity(0);
+    numberOfTeams = 0;
 }
 
 MainWindow::~MainWindow() { delete ui; }
@@ -26,6 +27,7 @@ void MainWindow::on_pushButton_home_clicked() { ui->stackedWidget->setCurrentInd
  * on_pushButton_eastDiv_clicked(), and
  * on_pushButton_westDiv_clicked() set tableView_conferences model to the returned QSqlQueryModel from the populateView function
  */
+
 void MainWindow::on_pushButton_viewAFC_clicked()
 {
     ui->tableView_conferences->setModel(dbQuery.populateView("American Football Conference", 0));
@@ -60,4 +62,83 @@ void MainWindow::on_pushButton_westDiv_clicked()
 {
     ui->tableView_conferences->setModel(dbQuery.populateView("West", 1));
     ui->tableView_conferences->setColumnWidth(0, ui->tableView_conferences->width());
+}
+
+void MainWindow::on_pushButton_Stadiums_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+    QSqlTableModel *stadiumsTable = new QSqlTableModel(this, dbmanager::instance().getDatabase());
+
+    stadiumsTable->setTable("TeamInfo");
+    stadiumsTable->setSort(1, Qt::SortOrder::AscendingOrder);
+    stadiumsTable->select();
+
+    ui->tableView_Stadiums->setModel(stadiumsTable);
+
+    for (int i = 2; i < 9; i++) { ui->tableView_Stadiums->hideColumn(i); }
+
+    ui->tableView_Stadiums->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableView_Stadiums->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView_Stadiums->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
+    ui->tableView_Stadiums->horizontalHeader()->swapSections(1, 0);
+}
+
+void MainWindow::on_pushButton_home_2_clicked() { ui->stackedWidget->setCurrentIndex(0); }
+
+void MainWindow::on_pushButton_home_3_clicked() { ui->stackedWidget->setCurrentIndex(0); }
+
+void MainWindow::on_pushButton_TeamInfo_clicked()
+{
+    populateTeamDropdown();
+    populateAllTeamInfo("All Teams");
+}
+
+void MainWindow::populateTeamDropdown()
+{
+    QSqlQuery query;
+    numberOfTeams = 0;
+
+    query.exec("SELECT TeamName FROM TeamInfo;");
+
+    ui->comboBox_teamDropdown->clear();
+
+    ui->comboBox_teamDropdown->addItem("All Teams");
+    numberOfTeams++;
+
+    while (query.next())
+    {
+        ui->comboBox_teamDropdown->addItem(query.value(0).toString());
+        numberOfTeams++;
+    }
+}
+
+void MainWindow::populateAllTeamInfo(const QString &selectedTeam)
+{
+    ui->stackedWidget->setCurrentIndex(3);
+
+    QSqlTableModel *allTeamInfoTable = new QSqlTableModel(this, dbmanager::instance().getDatabase());
+
+    allTeamInfoTable->setTable("TeamInfo");
+
+    if (selectedTeam != "All Teams")
+        allTeamInfoTable->setFilter("TeamName=\""+selectedTeam+"\"");
+    else
+        allTeamInfoTable->setFilter("");
+
+    allTeamInfoTable->setSort(4, Qt::SortOrder::AscendingOrder);
+
+    allTeamInfoTable->select();
+
+    ui->tableView_allTeamInfo->setModel(allTeamInfoTable);
+
+    ui->tableView_allTeamInfo->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableView_allTeamInfo->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView_allTeamInfo->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
+
+}
+
+void MainWindow::on_comboBox_teamDropdown_currentIndexChanged(const QString &arg1)
+{
+    if (ui->comboBox_teamDropdown->count() >= numberOfTeams)
+        populateAllTeamInfo(ui->comboBox_teamDropdown->currentText());
 }
